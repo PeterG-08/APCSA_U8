36e47795e0f7fb7830edd9cc8a231cc4dba82320
Jack <jackn25@nycstudents.net>
2025-03-12 13:18:56 -0400

    working

--- src/L1/Main.java
+++ src/L1/Main.java
@@ -1,16 +1,26 @@
 package L1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String[][] seatingChart = new String[][]{
-                {"Abby", "Don", "George", "Kim"},
-                {"Brian", "Elenor", "Harry", "Lenny"},
-                {"Cathy", "Fred", "Jill", "Matt"}
-        };
+        Integer[][] arr1 = new Integer[2][3];
+        Integer[][] arr2 = new Integer[3][2];
 
-        var temp = seatingChart[2][3];
+        arr1[0][0] = 1;
+        arr1[0][1] = 2;
+        arr1[0][2] = 3;
+        arr1[1][0] = 4;
+        arr1[1][1] = 5;
+        arr1[1][2] = 6;
 
-        seatingChart[2][3] = seatingChart[0][0];
-        seatingChart[0][0] = temp;
+        arr2[0][0] = 1;
+        arr2[1][0] = 2;
+        arr2[2][0] = 3;
+        arr2[0][1] = 4;
+        arr2[1][1] = 5;
+        arr2[2][1] = 6;
+
+        System.out.println(arr1[0][2] + arr2[2][0]);
     }
 }
